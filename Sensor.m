9680ec659d0aaa43fd8c458d90130214ab36d70a
Travis F. Collins <travis.collins@analog.com>
2019-08-06 12:23:49 -0700

    Update sensor API
    Signed-off-by: Travis F. Collins <travis.collins@analog.com>

--- Sensor.m
+++ Sensor.m
@@ -1,55 +1,72 @@
 classdef (Abstract) Sensor  < adi.common.RxTx & adi.common.Attribute
     % Sensor: Common shared functions between sensor classes
-    properties(Constant, Hidden, Logical)
-        EnableCyclicBuffers = false;
+    properties (Nontunable, Hidden)
+        %SamplesPerFrame Samples Per Frame
+        %   Number of samples per frame, specified as an even positive
+        %   integer from 2 to 16,777,216.
+        SamplesPerFrame = 1024;
     end
-    properties (Abstract, Hidden, Constant)
-        SensorAttributeNames
-        SensorAttributeTypes
+    properties (Nontunable)
+       %ReadMode Read Mode
+       %    Specify whether to return the latest or the oldest data
+       %    samples. The number of samples depends on the SamplesPerRead
+       %    value. The data read from the sensor is stored in the MATLAB
+       %    buffer.
+       ReadMode = 'oldest';
+       %   Set the output format of the data returned by executing the read
+       %   function. When the OutputFormat is set to timetable, the data
+       %   returned has the following fields (if supported by device):
+       %       Time — Time stamps in datetime or duration format
+       %       Acceleration — N-by-3 array in units of m/s^2
+       %       AngularVelocity — N-by-3 array in units of rad/s
+       %       MagneticField — N-by-3 array in units of µT (microtesla)
+       %   When the OutputFormat is set to matrix, the data is returned as
+       %   matrices of acceleration, angular velocity, magnetic field, and
+       %   time stamps. The units for the sensor readings are the same as
+       %   the timetable format. The size of each matrix is N-by-3. N is
+       %   the number of samples per read specified by SamplesPerRead. The
+       %   three columns of each field represent the measurements in x, y,
+       %   and z axes.
+       OutputFormat = 'matrix';
     end
-    properties (Abstract, Hidden)
-       SensorAttributeScales
+    
+    properties (Dependent, Nontunable)
+        %SamplesPerRead Samples Per Read
+        %   Number of samples per read, specified as a positive
+        %   integer.
+        SamplesPerRead
     end
+    
+    methods
+        function value = get.SamplesPerRead(obj)
+            value = obj.SamplesPerFrame;
+        end
+        function set.SamplesPerRead(obj,value)
+            obj.SamplesPerFrame = value;
+        end
+    end
+    
+    properties(Constant, Hidden)
+        ReadModeSet = matlab.system.StringSet({ ...
+            'oldest','latest'});
+        OutputFormatSet = matlab.system.StringSet({ ...
+            'timetable','matrix'});
+    end
+    
+    methods (Abstract)
+        read(obj);
+        flush(obj);
+    end
+        
     methods (Hidden, Access = protected)
-        
-        function varargout = stepImpl(obj)
-            % Get the data
-            c = length(obj.SensorAttributeNames);
-            data = zeros(obj.SamplesPerFrame,c);
-            for i = 1:obj.SamplesPerFrame
-                for k = 1:c
-                    switch obj.SensorAttributeTypes{k}
-                        case 'LongLong'
-                            data(i,k) = obj.getAttributeLongLong(...
-                                obj.SensorAttributeNames{k},'raw',0) * ...
-                                obj.SensorAttributeScales(k);
-                        case 'Double'
-                            data(i,k) = obj.getAttributeDouble(...
-                                obj.SensorAttributeNames{k},'raw',0) * ...
-                                obj.SensorAttributeScales(k);
-                        case 'Raw'
-                            data(i,k) = obj.getAttributeRAW(...
-                                obj.SensorAttributeNames{k},'raw',0);
-                            
-                    end
-                end
-            end
-            varargout = cell(c,1);
-            for k=1:c
-                varargout{k} = data(:,k);
-            end
-        end
-        
-        function scales = CollectScales(obj)
-            % Get the data
-            c = length(obj.SensorAttributeNames);
-            scales = zeros(c,1);
-            for k = 1:c
-                scales(k) = obj.getAttributeDouble(...
-                    obj.SensorAttributeNames{k},'scale',0);
-            end
-        end
-        
+       
+        function flag = isInactivePropertyImpl(obj, prop)
+            flag = isInactivePropertyImpl@adi.common.RxTx(obj, prop);
+            flag = flag || strcmpi(prop,'EnabledChannels');
+            flag = flag || strcmpi(prop,'SamplesPerFrame');
+            % NOT SUPPORTED YET
+            flag = flag || strcmpi(prop,'OutputFormat');
+        end 
     end
     
 end
