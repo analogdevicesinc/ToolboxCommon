4bc1e1621ddcee8b12a4ec588be31985e3fc82aa
Travis F. Collins <travis.collins@analog.com>
2020-09-08 12:11:37 -0700

    Add bypasses to read checks on debug attributes
    Signed-off-by: Travis F. Collins <travis.collins@analog.com>

--- DebugAttribute.m
+++ DebugAttribute.m
@@ -1,8 +1,12 @@
 classdef (Abstract) DebugAttribute < matlabshared.libiio.base 
     
     methods (Hidden)
-        function setDebugAttributeLongLong(obj,attr,value,phydev)
+        function setDebugAttributeLongLong(obj,attr,value,skipCheck,phydev)
             if nargin < 4
+                phydev = getDev(obj, obj.phyDevName);
+                skipCheck = false;
+            end
+            if nargin < 5
                 phydev = getDev(obj, obj.phyDevName);
             end
             if (nargin == 1)
@@ -12,16 +16,22 @@ classdef (Abstract) DebugAttribute < matlabshared.libiio.base
             status = iio_device_debug_attr_write_longlong(obj,phydev,attr,value);
             cstatus(obj,status,['Attribute write failed for : ' attr ' with value ' num2str(value)]);
             % Check
-            [status, rValue] = iio_device_debug_attr_read_longlong(obj,phydev,attr);
-            cstatus(obj,status,['Error reading attribute: ' attr]);
-            if (value ~= rValue)
-                status = -1;
-                cstatus(obj,status,['Attribute ' attr ' return value ' num2str(rValue) ', expected ' num2str(value)]);
-            end            
+            if ~skipCheck
+                [status, rValue] = iio_device_debug_attr_read_longlong(obj,phydev,attr);
+                cstatus(obj,status,['Error reading attribute: ' attr]);
+                if (value ~= rValue)
+                    status = -1;
+                    cstatus(obj,status,['Attribute ' attr ' return value ' num2str(rValue) ', expected ' num2str(value)]);
+                end
+            end
         end
         
-        function setDebugAttributeBool(obj,attr,value,phydev)
+        function setDebugAttributeBool(obj,attr,value,skipCheck,phydev)
             if nargin < 4
+                phydev = getDev(obj, obj.phyDevName);
+                skipCheck = false;
+            end
+            if nargin < 5
                 phydev = getDev(obj, obj.phyDevName);
             end
             if (nargin == 1)
